6e19835fea24438a27cd20e96cfecded1f4a2959
c0pperdragon <reinhard.grafl@aon.at>
2020-10-06 11:26:17 +0200

    Intel hex format output from byte assembler

--- ByteCPU/assembler/byteasm.py
+++ ByteCPU/assembler/byteasm.py
@@ -40,9 +40,9 @@ def evaluate(identifiers, s):
     elif len(s)>0 and s[0]=='^': 
         return (evaluate(identifiers, s[1:]) >> 8) & 0xff
     elif len(s)>0 and s[0]=='$':
-        return int(s[1:], 16) & 0xffff 
+        return int(s[1:], 16)
     else:
-        return int(s,10) & 0xffff
+        return int(s,10)
 
 def op(identifiers, tokens, generate, tidx, bitpos, bitlen):
     if tidx >= len(tokens):
@@ -88,13 +88,13 @@ def processline(identifiers, tokens, generate, codeaddress):
     elif len(tokens)==3 and tokens[1]=='=':
         if not generate:
             id = tokens[0]
-            value = evaluate(identifiers,tokens[2]);
+            value = op(I,T,G, 2, 0,16);
             if id in identifiers:
                 raise AssemblerException("May not redefine '"+id+"'")#
             else:
                 identifiers[id] = value
     elif len(tokens)==2 and tokens[0]=="ORG":
-        codeaddress[0] = evaluate(identifiers,tokens[1])
+        codeaddress[0] = op(I,T,G, 1, 0,16)
     elif tokens[0]=="BYTE":
         for idx in range(1,len(tokens)):
             bytes.append(op(I,T,G, idx, 0,8))
@@ -140,10 +140,12 @@ def processline(identifiers, tokens, generate, codeaddress):
     codeaddress[0] += len(bytes)
     return bytes        
                 
-def process(identifiers, sourcefile, generate):
+def process(identifiers, sourcefile, outbuffer):
+    generate = bool(outbuffer)
     src = open(sourcefile, "r")
     codeaddress = [0]
     linenumber = 1
+    numerrors = 0
     for rawline in src:
         line = rawline.rstrip()
         try:
@@ -152,16 +154,54 @@ def process(identifiers, sourcefile, generate):
             bytes = processline(identifiers, tokens, generate, codeaddress)
             if generate:
                 printlisting(startaddress, bytes, line)
+                outbuffer[startaddress:startaddress+len(bytes)] = bytes
         except AssemblerException as e:
             print(sourcefile+":"+str(linenumber)+" "+str(e),file=sys.stderr)
-        linenumber = linenumber+1
+            numerrors += 1
+        linenumber += linenumber
     src.close()
+    if numerrors>0:
+        raise AssemblerException("Encountered "+str(numerrors)+" errors")
 
+def formatwithchecksum(record):
+    line = ":"
+    sum = 0x00
+    for i in range(len(record)):#
+        b = record[i]
+        line = line + "{:02x}".format(b)
+        sum += b
+    return line + "{:02x}".format((-sum) & 0xff)
+        
+def printhexfile(hexfile, buffer):
+    dest = open(hexfile, "w")
+    start=0
+    while start<len(buffer):
+        if buffer[start] == None:
+            start += 1
+        else:
+            record = [ 0x00, (start>>8) & 0xff, start&0xff, 0x00 ]
+            l = 0
+            while l<16 and start+l<len(buffer) and buffer[start+l] != None:
+                record.append(buffer[start+l])
+                l += 1
+            record[0] = l
+            print(formatwithchecksum(record), file=dest)
+            start += l
+    print(formatwithchecksum([0x00,0x00,0x00,0x01]), file=dest)                
+    dest.close()
+        
 def asm(sourcefile,hexfile):
-    identifiers = { "A": 0, "B": 1, "C": 2, "D": 3 }
-    process(identifiers, sourcefile, False)
-#    print(identifiers)
-    process(identifiers, sourcefile, True)
+    try:
+        identifiers = { "A": 0, "B": 1, "C": 2, "D": 3 }
+        process(identifiers, sourcefile, None)
+        rom = [None]*65536
+        process(identifiers, sourcefile, rom)
+        printhexfile(hexfile, rom)
+    except AssemblerException as e:
+        print (e,file=sys.stderr) 
 
-f = "../samples/ledanimation"
-asm(f+".asm",f+".hex")
+if len(sys.argv)>=2: 
+    asm(sys.argv[1]+".asm",sys.argv[1]+".hex")
+else:
+    print("No filename given to assemble",file=sys.stderr)
+    
