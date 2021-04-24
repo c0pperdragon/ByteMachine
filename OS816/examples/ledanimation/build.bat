4b78bd04a9dd8f29c8151e1ceef4f4461698df39
c0pperdragon <reinhard.grafl@aon.at>
2021-04-24 21:57:49 +0200

    Transfer OS816 into its own repo.

--- OS816/examples/ledanimation/build.bat
+++
@@ -1,3 +0,0 @@
-WDC816CC -ML -MV -MU -MK -MT -SOP -WL -WP -I..\..\src ledanimation.c
-WDCLN -HIE -Areset=80FFF8,0FFF8 -D10000,00000 -U, -C810000,10000 -K, ledanimation.obj ..\..\bin\startup.obj -l..\..\bin\os816
-
